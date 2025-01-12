893099b256d17c860f67771aa58263c3eafd1cba
Artur Musaev <cormeum@MacBook-Air-Artur.local>
2025-01-12 16:56:33 +0300

    doc strings added

--- api/utils.py
+++ api/utils.py
@@ -14,6 +14,18 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 def initialize_llm_client(config: dict):
+    """
+    Initialize a client for interacting with the LLM.
+
+    Args:
+        config: A configuration dictionary containing the LLM's base URL and API key.
+
+    Returns:
+        An OpenAI client object.
+
+    Raises:
+        Exception: If the LLM client could not be initialized.
+    """
     try:
         llm_client = OpenAI(
             base_url=config["llm"]["base_url"], api_key=os.getenv("API_KEY")
@@ -25,6 +37,17 @@ def initialize_llm_client(config: dict):
         raise
 
 async def retrieve_embeddings(query: str, config: dict):
+    """
+    Asynchronously retrieve embeddings for a given query using a remote retrieval service.
+
+    Args:
+        query: A string containing the query for which embeddings are to be retrieved.
+        config: A dictionary containing configuration details, including the host and port of the retrieval service.
+
+    Returns:
+        A JSON response containing the embeddings for the query.
+    """
+
     retriever_url = f"http://{config['retrieval']['host']}:{config['retrieval']['port']}/similarity_search"
     async with httpx.AsyncClient() as client:
         response = await client.post(retriever_url, json={"query": query}, timeout=1000.0)
@@ -32,6 +55,16 @@ async def retrieve_embeddings(query: str, config: dict):
         return response.json()
     
 async def retrieve_ner(chunks: str, config: dict):
+    """
+    Asynchronously retrieve named entities for a given query using a remote retrieval service.
+
+    Args:
+        chunks: A string containing the query for which named entities are to be retrieved.
+        config: A dictionary containing configuration details, including the host and port of the retrieval service.
+
+    Returns:
+        A JSON response containing the named entities for the query.
+    """
     retriever_url = f"http://{config['retrieval']['host']}:{config['retrieval']['port']}/search_ner"
     async with httpx.AsyncClient() as client:
         response = await client.post(retriever_url, json={"query": chunks}, timeout=1000.0)
@@ -39,6 +72,14 @@ async def retrieve_ner(chunks: str, config: dict):
         return response.json()    
      
 def insert_embeddings_from_parquet_icd(file_path_parquet: str, file_path_npy: str, batch_size=1000):
+    """
+    Insert embeddings from Parquet and .npy files into the database.
+
+    Args:
+        file_path_parquet (str): The path to the Parquet file containing the embeddings.
+        file_path_npy (str): The path to the .npy file containing the embeddings.
+        batch_size (int, optional): The number of embeddings to insert in each batch. Defaults to 1000.
+    """
     session = SessionLocal()
     try:
         if session.query(ICDDocument.id).first() is not None:
@@ -75,6 +116,16 @@ def insert_embeddings_from_parquet_icd(file_path_parquet: str, file_path_npy: st
         logger.info("Session closed.")
 
 async def retrieve_icd(dis: str, config: dict):
+    """
+    Asynchronously retrieve ICD code for a given disease/disorder using a remote retrieval service.
+
+    Args:
+        dis (str): The disease/disorder for which the ICD code is to be retrieved.
+        config (dict): A dictionary containing configuration details, including the host and port of the retrieval service.
+
+    Returns:
+        A JSON response containing the ICD code for the given disease/disorder.
+    """
     retriever_url = f"http://{config['retrieval']['host']}:{config['retrieval']['port']}/similarity_search_icd"
     async with httpx.AsyncClient() as client:
         response = await client.post(retriever_url, json={"query": dis}, timeout=1000.0)
@@ -82,6 +133,15 @@ async def retrieve_icd(dis: str, config: dict):
         return response.json()
     
 def insert_embeddings_from_parquet(file_path_parquet: str, file_path_npy: str, batch_size=1000):
+    
+    """
+    Inserts embeddings from a Parquet file and a .npy file into the database in batches.
+
+    Args:
+        file_path_parquet (str): The path to the Parquet file containing the text data.
+        file_path_npy (str): The path to the .npy file containing the embeddings.
+        batch_size (int, optional): The number of rows to insert in each batch. Defaults to 1000.
+    """
     session = SessionLocal()
     try:
         if session.query(Document.id).first() is not None:
